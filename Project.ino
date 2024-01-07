52b4f87188b269b1c903add5753e8227cf0589a6
Lairlynx <76436063+Lairlynx@users.noreply.github.com>
2024-01-07 16:44:50 +0200

    Add files via upload

--- Project.ino
+++ Project.ino
@@ -5,9 +5,9 @@
 #include <Wire.h>
 #include <BH1750.h>
 
-#define DHT22_PIN 12
+#define DHT22_PIN 12    //Defines the pin used to read from the DHT22 sensor
 dht DHT;       // Creates a DHT object
-BH1750 lightMeter; // initialize BH1750 object
+BH1750 lightMeter;    // initialize BH1750 object for the light meter
 #define ONE_WIRE_BUS 11   // Define to which pin of the Arduino the 1-Wire bus is connected:
 OneWire oneWire(ONE_WIRE_BUS);    // Create a new instance of the oneWire class to communicate with any OneWire device:
 DallasTemperature sensors(&oneWire);    // Pass the oneWire reference to DallasTemperature library:
@@ -44,7 +44,8 @@ int selectedOption = 0;
 
 const int numOptions = 5;
 
-const char *menuOptions[] = {
+const char *menuOptions[] = 
+{
   "Temp Monitor",
   "Motor Control",
   "Light Monitor",
@@ -52,7 +53,9 @@ const char *menuOptions[] = {
   "Automatic"
 };
 
-void setup_timer(){
+// Setting up the registries for motor control
+void setup_timer()
+{
   TCCR1A = 0x00; // Registry initiaization
   TCCR1A |= (1<<5); //Clear OC1B on compare match, set OC1B at BOTTOM (non-inverting mode)
   TCCR1A |= (1<<1); //WGM11,
@@ -63,10 +66,11 @@ void setup_timer(){
   OCR1A = 160; // For a period of 10 microseconds and prescaler of 1
   OCR1B = 0; // For a starting duty cycle of 0%
   DDRB |= (1<<2); //DDRB |= 0b00000100 -- Setting pin PB2 corresponding to OC1B as output
-  }
+}
 
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);   //Initialize serial monitor
 
   setup_timer();    //Setup for Motor registers
@@ -84,7 +88,8 @@ void setup() {
 }
 
 //Loop function
-void loop() {
+void loop() 
+{
   switch (currentState) {
     case ACCESS_DENIED:
       handleAccessDenied();
@@ -106,7 +111,8 @@ void loop() {
 }
 
 //Refresh pentru LCD atunci cand se face scroll in meniu
-void clearAndUpdateDisplay() {
+void clearAndUpdateDisplay() 
+{
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Select option:");
@@ -120,17 +126,42 @@ void clearAndUpdateDisplay() {
   }
 }
 
+// Automatic Operation Mode
 void handleAuto()
 {
   lcd.clear();
   lcd.print("Automatic Mode");
+  Serial.println("Automatic Mode");
+  sensors.requestTemperatures();
+  float tempDigital = sensors.getTempCByIndex(0);
+  Serial.println(tempDigital);
+  
+  if(tempDigital > 23.00 && tempDigital < 27.00)
+  {
+    OCR1A = 160; // For a period of 10 microseconds and prescaler of 1
+    OCR1B = 120; // For a duty cycle of 75%
+  }
+  else if(tempDigital >= 27.00)
+  {
+    OCR1A = 160; // For a period of 10 microseconds and prescaler of 1
+    OCR1B = 155; // For a duty cycle of 95%
+  }
+  else if(tempDigital <= 23.00)
+  {
+    OCR1A = 160; // For a period of 10 microseconds and prescaler of 1
+    OCR1B = 0; // For a duty cycle of 0%
+  }
+  
+  delay(2000);
 }
 
 //Wrong Password was input
-void handleAccessDenied() {
+void handleAccessDenied() 
+{
   char key = keypad.getKey();
 
-  if (key) {
+  if (key) 
+  {
     Serial.println(key);
     lcd.setCursor(input_password.length(), 1);
     lcd.print('*');
@@ -163,8 +194,10 @@ void handleAccessDenied() {
   }
 }
 
+
 //Right Password was input
-void handleAccessGranted() {
+void handleAccessGranted() 
+{
   Serial.println("Access granted");
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -173,23 +206,31 @@ void handleAccessGranted() {
   clearAndUpdateDisplay();
 }
 
+
 //Menu access
-void handleMenu() {
+void handleMenu() 
+{
   char key = keypad.getKey();
-  if (key) {
+  if (key) 
+  {
   Serial.print("This is key: ");
   Serial.println(key);
-  if (key!='*') {
+  if (key!='*') 
+  {
     switch (key) {
-      case '2':{
-        if (selectedOption > 0) {
+      case '2':
+      {
+        if (selectedOption > 0) 
+        {
           selectedOption--;
         }
         clearAndUpdateDisplay();
         break;
       }
-      case '8':{
-        if (selectedOption <= numOptions - 1) {
+      case '8':
+      {
+        if (selectedOption < numOptions - 1) 
+        {
           selectedOption++;
         }
         clearAndUpdateDisplay();
@@ -202,11 +243,10 @@ void handleMenu() {
         // Handle menu option based on selectedOption
         switch (selectedOption) 
         {
-          
+          // Temperature Monitoring
           case 0:
           {
             Serial.println("Case 0 accessed");
-            // Temperature Monitoring
             lcd.clear();
             lcd.setCursor(2, 0);
             lcd.print("Temperature:");
@@ -218,11 +258,12 @@ void handleMenu() {
             lcd.print("* Digital");
             break;
           }
-          
+
+          // Motor Control selected
           case 1:
           {
             Serial.println("Case 1 accessed");
-            // Motor Control selected
+            
             Serial.println("factor 50%");
             TCCR1B |= (1<<0);
             OCR1A = 160; // For a period of 10 microseconds and prescaler of 1
@@ -240,11 +281,11 @@ void handleMenu() {
             delay(5000); 
             break;
           }
-          
+
+          // Light Level Monitoring
           case 2:
           {
             Serial.println("Case 2 accessed");
-            // Light Level Monitoring
             lcd.clear();
             lcd.setCursor(2, 0);
             lcd.print("Light Level:");
@@ -257,15 +298,15 @@ void handleMenu() {
             lcd.print(" lx");
             break;
           }
-          
+
+          // Temperature & Humidity Monitoring
           case 3:
           {
             Serial.println("Case 3 accessed");
-            // Sensor 3 Input selected, add your code here
             int readData = DHT.read22(DHT22_PIN);
 
-            float t = DHT.temperature;        // Read temperature
-            float h = DHT.humidity;           // Read humidity
+            float t = DHT.temperature;        // Read temperature from DHT22 sensor
+            float h = DHT.humidity;           // Read humidity from DHT22 sensor
 
             Serial.print("Temperature = ");
             Serial.print(t);
@@ -277,10 +318,20 @@ void handleMenu() {
             Serial.println("% ");
             Serial.println("");
 
-            delay(2000); // wait two seconds
+            //Printing the sensor values to the lcd display
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("Temp: ");
+            lcd.print(t);
+            lcd.setCursor(0, 1);
+            lcd.print("Humidity: ");
+            lcd.print(h);
+
+            //delay(2000); // wait two seconds
             break;
           } 
 
+          // Entering automatic mode and exiting the menu
           case 4:
           {
             currentState = AUTO;
@@ -301,7 +352,9 @@ void handleMenu() {
         }
     }
   
-  } else if (key == '*') {
+  } 
+  else if (key == '*') 
+  {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Access granted");
